85a51d5eb51f4b143beed58071b1190d9208cf86
gare15 <72866876+gare15@users.noreply.github.com>
2020-11-08 09:04:03 +0700

    Warna Kepribadian

---
+++ src/pbo10k/WarnaKepribadian.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo10k;
+
+/**
+ *
+ * @author tegar
+ * 
+ */
+public class WarnaKepribadian {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        WKepribadian orang = new WKepribadian();
+        orang.getNama();
+        orang.getWarna();
+        orang.getKepribadian();
+    }
+}
+   
